6c1c730869a7af5127cb5ce2f8a014770baead69
Makkuusen <makkuusen@users.noreply.github.com>
2018-12-01 00:57:47 +0100

    En rad

--- src/mainTest.java
+++ src/mainTest.java
@@ -2,5 +2,7 @@ public class mainTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("lol");
+
+        System.out.println("Skriver på min laptop");
     }
 }
