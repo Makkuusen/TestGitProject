53c3f7d7cf514145dc3e4bf19fa64044bf49eb17
Makkuusen <unconfigured@null.spigotmc.org>
2018-12-01 01:11:05 +0100

    Sidetrack

--- src/mainTest.java
+++ src/mainTest.java
@@ -2,6 +2,7 @@ public class mainTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("lol");
+<<<<<<< HEAD
 
         System.out.println("Skriver på min laptop");
         
@@ -10,5 +11,7 @@ public class mainTest {
         System.out.println("åäö");
         
         System.out.println("åäö");
+=======
+>>>>>>> parent of 6c1c730... En rad
     }
 }
